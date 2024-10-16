51f5502b502a334b6b68c7b2fb29d897583ed88b
emurphy42 <emurphy42@gmail.com>
2024-10-15 22:44:25 -0700

    Add project files.

---
+++ WhatDoesTrashBearWant/ModEntry.cs
@@ -0,0 +1,30 @@
+﻿using StardewModdingAPI;
+using HarmonyLib;
+
+namespace WhatDoesTrashBearWant
+{
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            ObjectPatches.ModInstance = this;
+
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+            // detect interaction with Trash Bear
+            harmony.Patch(
+                original: AccessTools.Method(typeof(StardewValley.Characters.TrashBear), nameof(StardewValley.Characters.TrashBear.checkAction)),
+                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.TrashBear_checkAction_Prefix))
+            );
+            // if interaction with Trash Bear caused desired item to be refreshed, then react to it
+            harmony.Patch(
+                original: AccessTools.Method(typeof(StardewValley.Characters.TrashBear), nameof(StardewValley.Characters.TrashBear.updateItemWanted)),
+                postfix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.TrashBear_updateItemWanted_Postfix))
+            );
+        }
+    }
+}
---
+++ WhatDoesTrashBearWant/ObjectPatches.cs
@@ -0,0 +1,37 @@
+﻿using StardewValley;
+using StardewModdingAPI;
+
+namespace WhatDoesTrashBearWant
+{
+    internal class ObjectPatches
+    {
+        // initialized by ModEntry.cs
+        public static ModEntry ModInstance;
+
+        public static bool fromCheckAction = false;
+
+        // detect interaction with Trash Bear
+        public static bool TrashBear_checkAction_Prefix(Farmer who, GameLocation l)
+        {
+            ModInstance.Monitor.Log($"[What Does Trash Bear Want] {who.Name} interacted with Trash Bear", LogLevel.Trace);
+            fromCheckAction = true;
+            return true; // run base game function normally
+        }
+
+        // if interaction with Trash Bear caused desired item to be refreshed, then react to it
+        public static void TrashBear_updateItemWanted_Postfix(StardewValley.Characters.TrashBear __instance)
+        {
+            ModInstance.Monitor.Log("[What Does Trash Bear Want] Trash Bear's desired item was refreshed", LogLevel.Trace);
+            if (fromCheckAction)
+            {
+                var itemWanted = ItemRegistry.GetDataOrErrorItem(__instance.itemWantedIndex);
+                ModInstance.Monitor.Log($"[What Does Trash Bear Want] Trash Bear wants {itemWanted.DisplayName}", LogLevel.Debug);
+                Game1.showGlobalMessage(string.Format(
+                    ModInstance.Helper.Translation.Get("Message"),
+                    itemWanted.DisplayName
+                ));
+            }
+            fromCheckAction = false;
+        }
+    }
+}
